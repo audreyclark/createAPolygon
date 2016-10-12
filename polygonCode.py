e5d230dfdf1ac3f2af99e046dfc15712cb73c391
Audrey Clark <audreyclark@mac.com>
2016-10-12 13:31:48 -0700

    Updated file name and spacing

--- polygonCode.py
+++ polygonCode.py
@@ -1,9 +1,14 @@
-#Audrey Clark Lab Time Tue @2pm
+#Audrey Clark
+
 import turtle #importing the turtle library to use the drawing tool
+
 wn = turtle.Screen() #creating screen to draw on
 polygon = turtle.Turtle() #creating turtle to draw with
+
+#taking in an int value that will be used to calculate angles to make the polygon
 side = int(input("How many sides would you like your polygon to have?"))
-#inputing an int value that will be used to calculate angles to make the polygon
+
+
 for i in range(side):
     polygon.left((360/side)) #calculating angles by dividing 360 by number of sides
     polygon.forward(50)
